393206664d18065ba5bef60e86bf30b88525061d
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2020-08-29 09:54:13 +0200

    84f32ed

---
+++ _nuxt/static/1598687643/projects/project-5/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1598687643",layout:"default",error:null,state:{menu:[{slug:"about",title:"About"},{slug:"projects",title:"Projects"},{slug:"contact",title:"Contact"}]},serverRendered:true,routePath:"\u002Fprojects\u002Fproject-5",config:{}};
